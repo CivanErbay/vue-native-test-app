17e95c4aa7fb3e8dc0430c32f12177084ca63d58
Civan <civan.erbay@web.de>
2021-06-06 19:55:14 +0200

    Setup first vue native project

---
+++ App.vue
@@ -0,0 +1,37 @@
+<template>
+  <view class="container">
+    <text class="text-color-primary">{{ message }}</text>
+    <button title="hannaaaaaaa" @press="exclaim" />
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "Hello my Name is",
+      messageArray: ['',' Hanna ',' and ',' Civan ', 'and'],
+      messageCounter: 0
+    };
+  },
+  methods: {
+    exclaim() {
+      if(this.messageCounter < 4) this.messageCounter += 1;
+      this.message += this.messageArray[this.messageCounter]
+    }
+  },
+};
+</script>
+
+<style>
+.container {
+  flex: 1;
+  background-color: green;
+  align-items: center;
+  justify-content: center;
+}
+.text-color-primary {
+  color: black;
+  font-size: 30;
+}
+</style>
